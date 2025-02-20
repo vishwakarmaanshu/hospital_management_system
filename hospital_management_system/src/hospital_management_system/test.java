77adf758cadb42a91d56513776810e1ef5162713
vishwakarmaanshu <145771009+vishwakarmaanshu@users.noreply.github.com>
2025-02-20 14:01:10 +0530

    Add files via upload

---
+++ hospital_management_system/src/hospital_management_system/test.java
@@ -0,0 +1,14 @@
+package hospital_management_system;
+
+import javax.swing.*;
+
+public class test extends JFrame {
+
+    test(){
+        setSize(500,500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new test();
+    }
+}
