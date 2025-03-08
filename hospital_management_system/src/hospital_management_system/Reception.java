ee7cbb32154ab5aa405d79432462b49eddc1b871
vishwakarmaanshu <145771009+vishwakarmaanshu@users.noreply.github.com>
2025-03-08 11:52:27 +0530

    Add files via upload

--- hospital_management_system/src/hospital_management_system/Reception.java
+++ hospital_management_system/src/hospital_management_system/Reception.java
@@ -41,7 +41,7 @@ public class Reception extends JFrame {
         btn1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new New_Patient();
             }
         });
 
